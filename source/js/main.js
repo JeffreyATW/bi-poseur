526063937cbdbfb4f3800b7a072fb944a4556e20
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2014-10-05 12:28:01 -0700

    Switching to vimeo player

--- source/js/main.js
+++ source/js/main.js
@@ -1,60 +1,56 @@
 //= require plugins
 //= require bower_components/jquery/dist/jquery
 //= require vendor/bootstrap.custom
+//= require vendor/froogaloop
 
 (function () {
-  /*var $hero = $('.hero');
-  window.onYouTubeIframeAPIReady = function () {
-    $('.hero__trailer').on('click', function (e) {
-      var player,
-        $playerClose = $('<button class="btn player__close">&times;</button>'),
-        $playerFade = $('<div class="player__fade"></div>'),
-        removeFade = function () {
-          $playerFade.remove();
-        },
-        destroyPlayer = function () {
-          player.destroy();
-          $playerClose.remove();
-          $.support.transition ?
-            $playerFade.one($.support.transition.end, removeFade).emulateTransitionEnd(1000) :
-            removeFade();
-          $hero.removeClass('hero--playing');
-        },
-        appendPlayer = function () {
-          $hero.append('<iframe id="player" class="player" type="text/html" width="100%" height="100%" src="' +
-            'http://www.youtube.com/embed/DXraeuq5Lhc?autoplay=1&autohide=1&enablejsapi=1&showinfo=0&modestbranding=1&rel=0&origin=' +
-            window.location.protocol + '//' + window.location.host +
-            '" frameborder="0">').append($playerClose)
+  var $hero = $('.hero');
 
-          $playerClose.on('click', destroyPlayer);
-
-          player = new YT.Player('player', {
-            events: {
-              'onStateChange': function (e) {
-                if (e.data === 0) {
-                  destroyPlayer();
-                }
-              }
-            }
-          });
-        };
-      e.preventDefault();
-
-      $hero.append($playerFade);
-      setTimeout(function () {
-        $hero.addClass('hero--playing');
-
+  $('.hero__trailer').on('click', function (e) {
+    var player,
+      $iframe = $('<iframe class="player" width="100%" height="100%" src="' +
+        window.location.protocol + '//player.vimeo.com/video/108026836?api=1">'),
+      $playerClose = $('<button class="btn player__close">&times;</button>'),
+      $playerFade = $('<div class="player__fade"></div>'),
+      removeFade = function () {
+        $playerFade.remove();
+      },
+      destroyPlayer = function () {
+        $iframe.remove();
+        $playerClose.remove();
         $.support.transition ?
-          $playerFade.one($.support.transition.end, appendPlayer).emulateTransitionEnd(1000) :
-          appendPlayer();
-      }, 1);
-    });
-  };
-  $('script').first().before('<script src="' + window.location.protocol + '//www.youtube.com/iframe_api">');
-  */
+          $playerFade.one($.support.transition.end, removeFade).emulateTransitionEnd(1000) :
+          removeFade();
+        $hero.removeClass('hero--playing');
+      },
+      appendPlayer = function () {
+        $hero.append($iframe).append($playerClose)
+
+        $playerClose.on('click', destroyPlayer);
+
+        player = $f($iframe[0]);
+
+        player.addEvent('ready', function () {
+          player.play();
+        });
+
+        player.addEvent('finish', destroyPlayer);
+      };
+    e.preventDefault();
+
+    $hero.append($playerFade);
+    setTimeout(function () {
+      $hero.addClass('hero--playing');
+
+      $.support.transition ?
+        $playerFade.one($.support.transition.end, appendPlayer).emulateTransitionEnd(1000) :
+        appendPlayer();
+    }, 1);
+  });
+
   var comingSoon = function () {
     alert('Coming soon!');
     return false;
-  }
-  $('.hero__trailer, .cta-btn').on('click', comingSoon);
+  };
+  $('.hero__cta').on('click', comingSoon);
 }());
